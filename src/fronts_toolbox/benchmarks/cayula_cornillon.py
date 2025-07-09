13e3c54bfd98773e5a64a7b182c1dd55a9399c0d
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-07-09 18:48:23 +0200

    Up CC benchmarks

--- src/fronts_toolbox/benchmarks/cayula_cornillon.py
+++ src/fronts_toolbox/benchmarks/cayula_cornillon.py
@@ -1,6 +1,7 @@
 """Benchmark for Heterogeneity-Index."""
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 from fronts_toolbox.benchmarks.fields import add_spikes, ideal_jet, sample, swap_noise
 from fronts_toolbox.cayula_cornillon import cayula_cornillon_numpy
@@ -30,12 +31,27 @@ def plot(sst, fronts, title: str, input_kw: dict | None = None) -> plt.Figure:
     return fig
 
 
-if __name__ == "__main__":
+def plot_one(sst, fronts, title: str, **kwargs) -> plt.Figure:
+    fig, ax = plt.subplots(figsize=(6, 5), layout="constrained", dpi=150)
+    fig.suptitle(title, weight="bold")
+
+    im_kw: dict = dict(origin="lower", extent=[0, 1, 0, 1])
+
+    cmap_fronts = ListedColormap([(0, 0, 0, 0), (0, 0, 0, 1)])
+
+    ax.imshow(sst, cmap="inferno", **im_kw, **kwargs)
+    ax.imshow(fronts, cmap=cmap_fronts, **im_kw, **kwargs)
+
+    ax.tick_params(labelleft=False, labelbottom=False)
+
+    return fig
+
+    # if __name__ == "__main__":
     ## Ideal jet
 
     sst = ideal_jet()
     fronts = cayula_cornillon_numpy(sst)
-    plot(sst, fronts, "Ideal jet")
+    plot_one(sst, fronts, "Ideal jet")
 
     ## Ideal jet with noise
     sst = ideal_jet()
@@ -43,18 +59,18 @@ if __name__ == "__main__":
     sst = swap_noise(sst)
     sst = add_spikes(sst)
     fronts = cayula_cornillon_numpy(sst)
-    plot(sst, fronts, "Idea jet with noise", input_kw=dict(vmin=vmin, vmax=vmax))
+    plot_one(sst, fronts, "Idea jet with noise", vmin=vmin, vmax=vmax)
 
     ## Sample MODIS
 
     sst = (
         sample("MODIS")
         .sst4.isel(time=2)
-        .sel(lat=slice(30, 10), lon=slice(-120, -100))
+        .sel(lat=slice(20, 10), lon=slice(-110, -100))
         .to_numpy()[::-1]
     )
     fronts = cayula_cornillon_numpy(sst)
-    plot(sst, fronts, "MODIS L3M")
+    plot_one(sst, fronts, "MODIS L3M")
 
     ## Sample MODIS with overlap
 
@@ -64,7 +80,7 @@ if __name__ == "__main__":
         .sel(lat=slice(20, 10), lon=slice(-110, -100))
         .to_numpy()[::-1]
     )
-    fronts = cayula_cornillon_numpy(sst, window_size=32, window_step=4)
+    fronts = cayula_cornillon_numpy(sst, window_size=32, window_step=16)
     plot(sst, fronts, "MODIS L3M with overlap")
 
     ## Sample CCI/C3S
@@ -75,7 +91,5 @@ if __name__ == "__main__":
         .sel(lat=slice(15, 55), lon=slice(-82, -40))
         .to_numpy()
     )
-    fronts = cayula_cornillon_numpy(sst)
-    plot(sst, fronts, "CCI/C3S L4")
-
-    plt.show()
+    fronts = cayula_cornillon_numpy(sst, bimodal_criteria=0.4)
+    plot_one(sst, fronts, "CCI/C3S L4")
