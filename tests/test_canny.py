3ed3ebb08879be5b56ba103b0caa7fad19b56522
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-07-15 03:16:32 +0200

    Add tests for canny

---
+++ tests/test_canny.py
@@ -0,0 +1,29 @@
+"""Test Canny edge detector functions."""
+
+import pytest
+from numpy.testing import assert_allclose
+
+from fronts_toolbox import canny
+from tests.core import (
+    Basic,
+    get_input_fixture,
+    sst_dask,
+    sst_numpy,
+    sst_xarray,
+)
+
+input = get_input_fixture(canny, "canny")
+
+
+@pytest.mark.parametrize("input", ["numpy", "dask", "xarray"], indirect=True)
+class TestComponents(Basic):
+    n_output = 1
+
+
+def test_dask_correctness(sst_numpy, sst_dask):
+    edges_numpy = canny.canny_numpy(sst_numpy, hysteresis=False)
+    edges_dask = canny.canny_dask(sst_dask, hysteresis=False)
+    assert_allclose(edges_dask, edges_numpy)
+
+
+# TODO: normalization
