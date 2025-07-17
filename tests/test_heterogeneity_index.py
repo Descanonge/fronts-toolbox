1cc9d7b23deadc2b52e5ee7a7838b6f5ebfec8c4
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-07-17 12:06:09 +0200

    Add normalization tests

--- tests/test_heterogeneity_index.py
+++ tests/test_heterogeneity_index.py
@@ -26,4 +26,35 @@ def test_dask_correctness(sst_numpy, sst_dask):
     assert_allclose(edges_dask, edges_numpy)
 
 
-# TODO: normalization
+@pytest.fixture()
+def components_numpy(sst_numpy):
+    return heterogeneity_index.components_numpy(sst_numpy, window_size=5)
+
+
+@pytest.fixture()
+def components_dask(sst_dask):
+    return heterogeneity_index.components_dask(sst_dask, window_size=5)
+
+
+@pytest.fixture()
+def components_xarray(sst_xarray):
+    return heterogeneity_index.components_xarray(sst_xarray, window_size=5)
+
+
+components = get_input_fixture(
+    heterogeneity_index, "coefficients_components", fixture="components"
+)
+
+
+@pytest.mark.parametrize("components", ["numpy", "dask", "xarray"], indirect=True)
+class TestNormalization:
+    def test_components(self, components):
+        coefs = components.func(components.field)
+        assert list(coefs.keys()) == heterogeneity_index.COMPONENTS_NAMES
+        assert all(isinstance(c, float) for c in coefs.values())
+
+    def test_hi(self, components):
+        coefs = components.func(components.field)
+        coef_hi = heterogeneity_index.coefficient_hi(components.field, coefs)
+        assert isinstance(coef_hi, float)
+        assert coef_hi > 0.0
